de2ab074c4f2a1a5f470eac464c3babf9cc50935
Thomas Bueno <thomasscolax@gmail.com>
2020-03-23 09:51:30 -0300

    checking status -> production,development,test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def environment_rails
+        if Rails.env.development?
+            "Development"
+        elsif Rais.env.production?
+            "Production"
+        else
+            "test"
+        end
+    end
 end
